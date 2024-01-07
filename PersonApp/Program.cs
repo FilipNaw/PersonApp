db4264d684059320a168cf78e8593e1ade782a64
FilipN <filiplondon11@Gmail.com>
2024-01-07 12:21:22 +0100

    Add project files.

---
+++ PersonApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using PersonApp;
+using System;
+
+namespace PersonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.WriteLine("Date of birth: " + person.BirthDate);
+            Console.WriteLine("Gender: " + person.GetGender);
+
+            Console.ReadLine();
+        }
+    }
+}
